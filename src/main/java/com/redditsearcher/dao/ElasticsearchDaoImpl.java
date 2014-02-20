678e66f5115bc44a12d05bdc07299fa4bc0c4495
Vladmir Parshikov <v-vparshikov@expedia.com>
2014-02-20 14:18:58 +0000

    wip

--- src/main/java/com/redditsearcher/dao/ElasticsearchDaoImpl.java
+++ src/main/java/com/redditsearcher/dao/ElasticsearchDaoImpl.java
@@ -1,11 +1,12 @@
 package com.redditsearcher.dao;
 
-import java.util.List;
-
 import static org.elasticsearch.index.query.QueryBuilders.simpleQueryString;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
@@ -22,8 +23,16 @@ public class ElasticsearchDaoImpl implements ElasticsearchDao {
 
     @Override
     public List<Link> search(String query) {
+
+//        String script = "(0.08 / ((3.16*pow(10,-11)) * abs(currenttimeinmillis - doc['date'].date.getMillis()) + 0.05)) + 1.0";
+//        Map<String, Object> params = new HashMap<String, Object>();
+//        params.put("currenttimeinmillis", (Long) new Date().getTime());
+//        FunctionScoreQueryBuilder functionScoreQueryBuilder = QueryBuilders.functionScoreQuery(QueryBuilders.matchAllQuery())
+//                .add(ScoreFunctionBuilders.scriptFunction(script, params));
+
         SearchQuery searchQuery = new NativeSearchQueryBuilder()
             .withQuery(simpleQueryString(query))
+            .withPageable(new PageRequest(0, 100))
             .build();
 
         Page<ElasticLink> elasticLinks = elasticsearchTemplate.queryForPage(searchQuery, ElasticLink.class);
