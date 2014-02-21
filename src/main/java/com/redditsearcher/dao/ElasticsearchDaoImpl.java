e87243eb8190bf02cccb63df4553156a9e9808b1
Vladmir Parshikov <v-vparshikov@expedia.com>
2014-02-21 17:00:06 +0000

    added created date (needed for ranking)

--- src/main/java/com/redditsearcher/dao/ElasticsearchDaoImpl.java
+++ src/main/java/com/redditsearcher/dao/ElasticsearchDaoImpl.java
@@ -1,9 +1,17 @@
 package com.redditsearcher.dao;
 
-import static org.elasticsearch.index.query.QueryBuilders.simpleQueryString;
+import static org.elasticsearch.index.query.QueryBuilders.functionScoreQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.elasticsearch.index.query.MatchQueryBuilder.Operator;
+import org.elasticsearch.index.query.MatchQueryBuilder.Type;
+import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
+import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -23,17 +31,22 @@ public class ElasticsearchDaoImpl implements ElasticsearchDao {
 
     @Override
     public List<Link> search(String query) {
+        String scriptRecency = "(0.08 / ((3.16*pow(10,-11)) * abs(currenttimeinmillis - doc['created'].date.getMillis()) + 0.05)) + 1.0";
+        String scriptRating = "doc['rating'].value";
 
-//        String script = "(0.08 / ((3.16*pow(10,-11)) * abs(currenttimeinmillis - doc['date'].date.getMillis()) + 0.05)) + 1.0";
-//        Map<String, Object> params = new HashMap<String, Object>();
-//        params.put("currenttimeinmillis", (Long) new Date().getTime());
-//        FunctionScoreQueryBuilder functionScoreQueryBuilder = QueryBuilders.functionScoreQuery(QueryBuilders.matchAllQuery())
-//                .add(ScoreFunctionBuilders.scriptFunction(script, params));
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("currenttimeinmillis", (Long) new Date().getTime());
+
+        FunctionScoreQueryBuilder functionScoreQueryBuilder = functionScoreQuery(matchQuery("text", query)
+                .type(Type.BOOLEAN)
+                .operator(Operator.AND))
+                .add(ScoreFunctionBuilders.scriptFunction(scriptRecency, params))
+                .add(ScoreFunctionBuilders.scriptFunction(scriptRating));
 
         SearchQuery searchQuery = new NativeSearchQueryBuilder()
-            .withQuery(simpleQueryString(query))
-            .withPageable(new PageRequest(0, 100))
-            .build();
+                .withQuery(functionScoreQueryBuilder)
+                .withPageable(new PageRequest(0, 100))
+                .build();
 
         Page<ElasticLink> elasticLinks = elasticsearchTemplate.queryForPage(searchQuery, ElasticLink.class);
         List<Link> links = elasticsearchConverter.convertList(elasticLinks.getContent());
