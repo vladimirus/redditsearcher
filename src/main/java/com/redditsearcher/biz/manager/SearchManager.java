9d8aaeebf2388c01f42c5a01081e3a00631bfef7
Vladmir Parshikov <v-vparshikov@expedia.com>
2014-05-02 12:22:27 +0100

    test

--- src/main/java/com/redditsearcher/biz/manager/SearchManager.java
+++ src/main/java/com/redditsearcher/biz/manager/SearchManager.java
@@ -1,10 +1,9 @@
 package com.redditsearcher.biz.manager;
 
-import java.util.List;
-
 import com.redditsearcher.model.Link;
 
-public interface SearchManager {
+import java.util.List;
 
+public interface SearchManager {
     List<Link> search(String query);
 }
